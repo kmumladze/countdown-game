b747a13e00b9432676a71760444aae5e333f32d7
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-01-16 20:16:19 +0400

    feat: change open attribute to showModal method

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
-export default function ResultModal({ result, targerTime }) {
+export default function ResultModal({ ref, result, targerTime }) {
   return (
-    <dialog className="result-modal" open>
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targerTime} seconds.</strong>
