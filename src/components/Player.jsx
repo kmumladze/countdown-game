7c14ac1cafcb5cdf31238537eb18d30e3ccb3b2f
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-01-16 14:03:34 +0400

    fix: update keystroke

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,6 +5,7 @@ export default function Player() {
   const [submitted, setSubmitted] = useState(false);
 
   function handleChange(event) {
+    setSubmitted(false);
     setEnteredPlayerName(event.target.value);
   }
 
